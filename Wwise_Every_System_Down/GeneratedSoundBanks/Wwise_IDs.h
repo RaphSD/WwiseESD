5acb83769ce12fb53b5d7d8daadfbfd3abf50afe
RaphSD <33368306+RaphSD@users.noreply.github.com>
2018-04-26 10:42:24 +0200

    Add auro headphones for binaural

--- Wwise_Every_System_Down/GeneratedSoundBanks/Wwise_IDs.h
+++ Wwise_Every_System_Down/GeneratedSoundBanks/Wwise_IDs.h
@@ -42,6 +42,7 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID MAIN = 3161908922U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID SFX = 393239870U;
